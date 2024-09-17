f19f4ccb52ac4fc8fc7e517f99c9ade9dc73a8ac
XSolis <xochith.solis@gmail.com>
2024-09-16 19:10:23 -0500

    added simple UI _ X Branch

---
+++ testinggodot/scripts/MainMenu.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _delta_process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://CreateDeck.tscn")
+	pass # Replace with function body.
