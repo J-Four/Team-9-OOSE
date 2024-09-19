38a6ef2dbc23e79454851d9bd9045e30517b9ca5
XSolis <xochith.solis@gmail.com>
2024-09-18 21:04:17 -0500

    added more screens and movement between

--- testinggodot/scripts/MainMenu.gd
+++ testinggodot/scripts/MainMenu.gd
@@ -14,3 +14,38 @@ func _delta_process(delta: float) -> void:
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://CreateDeck.tscn")
 	pass # Replace with function body.
+
+
+func _on_deck_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://playDeck.tscn")
+	pass # Replace with function body.
+
+
+func _on_deck_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://playDeck.tscn")
+	pass # Replace with function body.
+
+
+func _on_deck_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://playDeck.tscn")
+	pass # Replace with function body.
+
+
+func _on_deck_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://playDeck.tscn")
+	pass # Replace with function body.
+
+
+func _on_deck_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://playDeck.tscn")
+	pass # Replace with function body.
+
+
+func _on_deck_6_pressed() -> void:
+	get_tree().change_scene_to_file("res://playDeck.tscn")
+	pass # Replace with function body.
+
+
+func _on_edit_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://EditDecks.tscn")
+	pass # Replace with function body.
---
+++ testinggodot/scripts/playScreen.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _Exit_deck_on_pressed() -> void:
+	get_tree().change_scene_to_file("res://MainSceneControl.tscn")
+	pass # Replace with function body.
